ac9017fc05dc4a7f84666baa3c183e048b363ca9
Jerzy Chalupski <jerzy@futuresimple.com>
2013-05-28 00:29:55 +0200

    Initial commit

---
+++ readme.md
@@ -0,0 +1,113 @@
+MicroOrm
+========
+
+A library for creating Data Access Objects from Android Cursors and converting
+them back into ContentValues.
+
+The following pattern is quite common:
+
+```java
+private static import MyContract.SomeObjectColumns.*;
+
+private static class SomeObject {
+
+  public final String mSomeField;
+  public final long mSomeOtherField;
+  public final Long mSomeOptionalField;
+  public final boolean mBooleanField;
+
+  private SomeObject (String someValue, long someOtherValue, Long someOptionalValue, boolean booleanValue) {
+    mSomeField = someValue;
+    mSomeOtherField = someOtherValue;
+    mSomeOptionalField = someOptionalValue;
+    mBooleanField = booleanValue;
+  }
+
+  public ContentValues getContentValues() {
+    ContentValues values =  new ContentValues();
+
+    values.put(SOME_FIELD, mSomeField);
+    values.put(SOME_OTHER_FIELD, mSomeOtherField);
+    values.put(SOME_OPTIONAL_FIELD, mSomeOptionalField);
+    values.put(SOME_BOOLEAN_FIELD, mBooleanField);
+
+    return values;
+  }
+
+  public static SomeObject fromCursor(Cursor c) {
+    String someValue = c.getString(c.getColumnIndexOrThrow(SOME_FIELD));
+    long someOtherValue = c.getLong(c.getColumnIndexOrThrow(SOME_OTHER_FIELD));
+
+    int optionalValueColumnIndex = c.getColumnIndexOrThrow(SOME_OPTIONAL_FIELD);
+    someOptionalValue = c.isNull(optionalValueColumnIndex)
+      ? null
+      : c.getLong(optionalValueColumnIndex);
+
+    booleanValue = c.getInt(c.getColumnIndexOrThrow(SOME_BOOLEAN_FIELD)) == 1;
+
+    return new SomeObject(someValue, someOtherValue, someOptionalValue, booleanValue);
+  }
+}
+```
+
+Kind of meh, especially if you repeat it several times. With MicroOrm you can
+reduce the boilerplate to this:
+
+```java
+private static import MyContract.SomeObjectColumns.*;
+
+private static class SomeObject {
+  @Column(SOME_FIELD)
+  public final String mSomeField;
+
+  @Column(SOME_OTHER_FIELD)
+  public final long mSomeOtherField;
+
+  @Column(SOME_OPTIONAL_FIELD)
+  public final Long mSomeOptionalField;
+
+  @Column(SOME_BOOLEAN_FIELD)
+  public final boolean mBooleanField;
+}
+```
+
+And then use uORM:
+
+```java
+MicroOrm uOrm = new MicroOrm();
+SomeObject o = uOrm.fromCursor(c, SomeObject.class);
+ContentValues values = uOrm.toContentValues(o);
+
+// in case you'll iterate over the whole cursor
+SomeObject o = new SomeObject();
+do {
+  uORM.fromCursor(c, o);
+} while (c.moveToNext());
+
+// if you need to dump the whole cursor to collection
+Collection<SomeObject> someObjects = uOrm.collectionFromCursor(c, SomeObject.class);
+```
+
+Caveats
+-------
+
+. Only basic data types (primitives, boxed primitives and Strings) are supported.
+. Unlike gson, MicroOrm works only on explicitly annotated fields.
+. Current implementation is roughly 2-2.5 times slower than handrolled methods.
+
+License
+-------
+
+    Copyright (C) 2013 Jerzy Chalupski
+
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+
+         http://www.apache.org/licenses/LICENSE-2.0
+
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
---
+++ src/com/chalup/microorm/MicroOrm.java
@@ -0,0 +1,151 @@
+/*
+ * Copyright (C) 2013 Jerzy Chalupski
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.chalup.microorm;
+
+import com.chalup.microorm.annotations.Column;
+import com.google.common.collect.ImmutableMap;
+import com.google.common.collect.Lists;
+import com.google.common.collect.Maps;
+
+import android.content.ContentValues;
+import android.database.Cursor;
+
+import java.lang.reflect.Field;
+import java.util.Collection;
+import java.util.Map;
+
+public class MicroOrm {
+  /**
+   * Creates an object of the specified type from the current row in
+   * {@link Cursor}.
+   *
+   * @param <T> the type of the desired object
+   * @param c a valid {@link Cursor}
+   * @param klass The {@link Class} of the desired object
+   * @return an object of type T created from the current row in {@link Cursor}
+   */
+  public <T> T fromCursor(Cursor c, Class<T> klass) {
+    TypeAdapter<T> adapter = getAdapter(klass);
+    return adapter.fromCursor(c, adapter.createInstance());
+  }
+
+  /**
+   * Fills the field in the provided object with data from the current row in
+   * {@link Cursor}.
+   *
+   * @param <T> the type of the provided object
+   * @param c a valid {@link Cursor}
+   * @param object the instance to be filled with data
+   * @return the same object for easy chaining
+   */
+  @SuppressWarnings("unchecked")
+  public <T> T fromCursor(Cursor c, T object) {
+    return ((TypeAdapter<T>) getAdapter(object.getClass())).fromCursor(c, object);
+  }
+
+  /**
+   * Creates the {@link ContentValues} from the provided object.
+   *
+   * @param <T> the type of the provided object
+   * @param object the object to be converted into {@link ContentValues}
+   * @return the {@link ContentValues} created from the provided object
+   */
+  @SuppressWarnings("unchecked")
+  public <T> ContentValues toContentValues(T object) {
+    return ((TypeAdapter<T>) getAdapter(object.getClass())).toContentValues(object);
+  }
+
+  /**
+   * Convenience method for converting the whole {@link Cursor} into
+   * {@link Collection} of objects of specified type.
+   *
+   * @param <T> the type of the provided object
+   * @param c a valid {@link Cursor}
+   * @param klass The {@link Class} of the desired object
+   * @return the {@link Collection} of object of type T created from the entire
+   *         {@link Cursor}
+   */
+  public <T> Collection<T> collectionFromCursor(Cursor c, Class<T> klass) {
+    Collection<T> result = Lists.newArrayList();
+
+    if (c != null) {
+      try {
+        if (c.moveToFirst()) {
+          TypeAdapter<T> adapter = getAdapter(klass);
+          do {
+            result.add(adapter.fromCursor(c, adapter.createInstance()));
+          } while (c.moveToNext());
+        }
+      } finally {
+        c.close();
+      }
+    }
+
+    return result;
+  }
+
+  @SuppressWarnings("unchecked")
+  private <T> TypeAdapter<T> getAdapter(Class<T> klass) {
+    TypeAdapter<?> cached = mTypeAdapterCache.get(klass);
+    if (cached != null) {
+      return (TypeAdapter<T>) cached;
+    }
+
+    TypeAdapter<T> adapter = buildTypeAdapter(klass);
+    mTypeAdapterCache.put(klass, adapter);
+    return adapter;
+  }
+
+  private <T> TypeAdapter<T> buildTypeAdapter(Class<T> klass) {
+    Collection<FieldAdapter> fieldAdapters = Lists.newArrayList();
+
+    for (Field field : klass.getFields()) {
+      Column annotation = field.getAnnotation(Column.class);
+      if (annotation != null) {
+        fieldAdapters.add(FIELD_ADAPTER_FACTORIES.get(field.getType()).buildAdapter(field));
+      }
+    }
+
+    return new ReflectiveTypeAdapter<T>(klass, fieldAdapters);
+  }
+
+  private static Map<Class<?>, FieldAdapterFactory> FIELD_ADAPTER_FACTORIES;
+
+  static {
+    Map<Class<?>, FieldAdapterFactory> factories = Maps.newHashMap();
+
+    factories.put(short.class, FieldAdapters.SHORT_FACTORY);
+    factories.put(int.class, FieldAdapters.INT_FACTORY);
+    factories.put(long.class, FieldAdapters.LONG_FACTORY);
+    factories.put(boolean.class, FieldAdapters.BOOLEAN_FACTORY);
+    factories.put(float.class, FieldAdapters.FLOAT_FACTORY);
+    factories.put(double.class, FieldAdapters.DOUBLE_FACTORY);
+
+    factories.put(Short.class, FieldAdapters.BOXED_SHORT_FACTORY);
+    factories.put(Integer.class, FieldAdapters.BOXED_INT_FACTORY);
+    factories.put(Long.class, FieldAdapters.BOXED_LONG_FACTORY);
+    factories.put(Boolean.class, FieldAdapters.BOXED_BOOLEAN_FACTORY);
+    factories.put(Float.class, FieldAdapters.BOXED_FLOAT_FACTORY);
+    factories.put(Double.class, FieldAdapters.BOXED_DOUBLE_FACTORY);
+
+    factories.put(String.class, FieldAdapters.STRING_FACTORY);
+
+    FIELD_ADAPTER_FACTORIES = ImmutableMap.copyOf(factories);
+  }
+
+  private Map<Class<?>, TypeAdapter<?>> mTypeAdapterCache = Maps.newHashMap();
+}
